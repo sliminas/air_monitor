b5ab09623637b93e05e81d6e948a7ed2537f0123
Stephan Leibelt <sliminas.bodo@gmail.com>
2019-08-07 22:33:01 +0200

    Make max sequel connections depending on puma thread count

--- config/database.rb
+++ config/database.rb
@@ -15,8 +15,7 @@ class Database
       connection = ENV['DATABASE_URL'] ||
         'postgres://rails:rails@localhost/air_monitor_development'
 
-      concurrent_threads = (ENV['WEB_CONCURRENCY'] || 2) * (ENV['RAILS_MAX_THREADS'] || 5)
-      Sequel.connect(connection, max_connections: 100, logger: Logger.new(log_file))
+      Sequel.connect(connection, max_connections: ENV['RAILS_MAX_THREADS'] || 5, logger: Logger.new(log_file))
     end
 
     def check_pending_migrations
