505be9483e8919678b7c1c88e9436f345f48e012
Stephan Leibelt <sliminas.bodo@gmail.com>
2019-08-07 22:23:08 +0200

    Try to fix connection issue

--- config/puma.rb
+++ config/puma.rb
@@ -8,6 +8,10 @@ rackup      DefaultRackup
 port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
+before_fork do
+  Sequel::Model.db.disconnect if defined?(Sequel::Model)
+end
+
 on_worker_boot do
   # Worker specific setup for Rails 4.1+
   # See: https://devcenter.heroku.com/articles/deploying-rails-applications-with-the-puma-web-server#on-worker-boot
